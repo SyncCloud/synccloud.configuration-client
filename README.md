ab4bf98dd55844230fd0921a95b9dd2a74ee8ce8
Nick Delitski <ndelitski@gmail.com>
2015-07-28 18:47:20 +0300

    fetch config from manager

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ROADMAP
-- fetch configuration from host
+- (+) fetch configuration from host
 - receive notification when configuration has changed on the host(and restart/update running service)
 
---
+++ test/client.js
@@ -0,0 +1,51 @@
+var expect = require('chai').expect,
+    $url = require('url'),
+    BASE_URL = 'http://synccloud-config.elasticbeanstalk.com/',
+    error = require('../lib/error'),
+    Client = require('../lib/client');
+
+describe('synccloud-configration-client', function() {
+	describe('fetch', function() {
+        var configUrl = $url.resolve(BASE_URL, '/config/unknown-service/master@test'),
+            expectedConfig = {
+                backend: 'http://google.com'
+            };
+
+		it('should fetch config with a given url', function(done) {
+			Client()
+                .fetch(configUrl)
+                .then(function (config) {
+                    expect(config).to.deep.equal(expectedConfig);
+                    done();
+                }, done);
+		});
+
+        it('should initialize with an url and fetch without arguments', function(done) {
+            Client({url: configUrl})
+                .fetch()
+                .then(function (config) {
+                    expect(config).to.deep.equal(expectedConfig);
+                    done();
+                }, done);
+        });
+
+        it('should fetch by specifying service parameters not full url', function(done) {
+            Client({url: BASE_URL})
+                .fetch({service: 'unknown-service', version: 'master', env: 'test'})
+                .then(function (config) {
+                    expect(config).to.deep.equal(expectedConfig);
+                    done();
+                }, done);
+        });
+
+        it('should fail with bad_url', function(done) {
+            //check for error code matching
+        	expect(function() { Client().fetch(); }).to.throw(error);
+        	expect(function() { Client().fetch({service: 'a'}); }).to.throw(error);
+        	expect(function() { Client().fetch({env: 'a'}); }).to.throw(error);
+        	expect(function() { Client().fetch({version: 'a'}); }).to.throw(error);
+        	expect(function() { Client().fetch({service: 'a', env: 'b', version: 'c'}); }).to.throw(error);
+            done();
+        });
+	});
+});
