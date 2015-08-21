ef72649d44284d7721cb6c4456881512370483e5
Nick Delitski <ndelitski@gmail.com>
2015-08-21 19:45:30 +0300

    connect with sockets

--- test/client.js
+++ test/client.js
@@ -11,17 +11,6 @@ describe('synccloud-configration-client', function() {
                 backend: 'http://google.com'
             };
 
-		it('should fetch config with a given url', function(done) {
-			Client()
-                .fetch(configUrl)
-                .then(function (config) {
-                    expect(config).to.deep.equal(expectedConfig);
-                    console.log('DFSF');
-                    done();
-                }, done)
-                .catch(done);
-		});
-
         it('should initialize with an url and fetch without arguments', function(done) {
             Client({url: configUrl})
                 .fetch()
@@ -31,35 +20,27 @@ describe('synccloud-configration-client', function() {
                 }, done)
                 .catch(done);
         });
-
-        it('should fetch by specifying service parameters not full url', function(done) {
-            Client({url: BASE_URL})
-                .fetch({service: 'test-service', version: 'master', env: 'production'})
-                .then(function (config) {
-                    expect(config).to.deep.equal(expectedConfig);
+        it('should connect to same socket', function(done) {
+            var c = Client({url: configUrl});
+            c.connect().then(function (s1) {
+                c.connect().then(function (s2) {
+                    expect(s1).to.equal(s2);
                     done();
-                }, done)
-                .catch(done);
+                });
+            })
         });
 
-        it('should fail with bad_url', function(done) {
-            //check for error code matching
-            expect(function() { Client().fetch(); }).to.throw(error);
-        	expect(function() { Client().fetch({service: 'a'}); }).to.throw(error);
-        	expect(function() { Client().fetch({env: 'a'}); }).to.throw(error);
-        	expect(function() { Client().fetch({version: 'a'}); }).to.throw(error);
-        	expect(function() { Client().fetch({service: 'a', env: 'b', version: 'c'}); }).to.throw(error);
-            done();
-        });
 	});
 
-    describe('heartbeat', function() {
-    	it('should heartbeat without error', function(done) {
-            Client()
-                .heartbeat($url.resolve(BASE_URL, '/heartbeat'), {name: 'some-service', pid: 512})
-                .then(function () {
-                    done();
-                })
-    	});
+    describe('parse config url', function() {
+    	it('should parse', function(done) {
+            var url = $url.resolve(BASE_URL, '/config/test-service/master@production');
+    		var configInfo = Client.prototype.parseConfigUrl(url);
+            expect(configInfo).to.have.property('baseUrl', BASE_URL);
+            expect(configInfo).to.have.property('service', 'test-service');
+            expect(configInfo).to.have.property('version', 'master');
+            expect(configInfo).to.have.property('env', 'production');
+            done();
+        });
     });
 });
